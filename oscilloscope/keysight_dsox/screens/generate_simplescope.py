51104795a6acf731efd6f411f5e6174a19f43836
Sukhanov, Andrei <sukhanov@bnl.gov>
2026-08-17 09:55:30 -0400

    1st manual update

--- oscilloscope/keysight_dsox/screens/generate_simplescope.py
+++ oscilloscope/keysight_dsox/screens/generate_simplescope.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import phoebusgen.screen
 import phoebusgen.widget
 
+PREFIX = "$(DEV):"
 
 def main() -> None:
     screen = phoebusgen.screen.Screen("simplescope", "simplescope.bob")
@@ -13,31 +14,38 @@ def main() -> None:
 
     widgets = [
         phoebusgen.widget.Label("title", "SimpleScope", 20, 10, 220, 30),
-        phoebusgen.widget.Label("idn_lbl", "Scope ID", 20, 52, 140, 24),
-        phoebusgen.widget.TextUpdate("idn", "KDSOX:scopeIDN", 170, 50, 780, 26),
+        phoebusgen.widget.TextUpdate("idn", "${PREFIX}scopeIDN", 120, 10, 270, 20),
+        phoebusgen.widget.TextUpdate("vers", "${PREFIX}VERSION", 410, 10, 140, 20),
 
-        phoebusgen.widget.Label("state_lbl", "Trigger State", 20, 88, 140, 24),
-        phoebusgen.widget.TextUpdate("state", "KDSOX:trigState", 170, 86, 180, 26),
+        phoebusgen.widget.Label("state_lbl", "Trig State:", 20, 38, 70, 20),
+        phoebusgen.widget.TextUpdate("state", "${PREFIX}trigState", 170, 38, 70, 20),
 
-        phoebusgen.widget.Label("acq_lbl", "Acq Count", 380, 88, 120, 24),
-        phoebusgen.widget.TextUpdate("acq", "KDSOX:acqCount", 500, 86, 110, 26),
+        phoebusgen.widget.Label("acq_lbl", "Acq Count:", 380, 38, 80, 20),
+        phoebusgen.widget.TextUpdate("acq", "${PREFIX}acqCount", 460, 38, 60, 20),
 
-        phoebusgen.widget.Label("lost_lbl", "Lost Trigs", 640, 88, 120, 24),
-        phoebusgen.widget.TextUpdate("lost", "KDSOX:lostTrigs", 760, 86, 110, 26),
+        phoebusgen.widget.Label("lost_lbl", "Lost Trigs:", 540, 38, 70, 20),
+        phoebusgen.widget.TextUpdate("lost", "${PREFIX}lostTrigs", 620, 38, 60, 20),
 
-        phoebusgen.widget.Label("run_lbl", "Run/Stop", 20, 124, 140, 24),
-        phoebusgen.widget.ComboBox("run", "KDSOX:instrCtrl", 170, 122, 180, 26),
+        phoebusgen.widget.Label("run_lbl", "Run/Stop:", 20, 74, 70, 20),
+        phoebusgen.widget.ComboBox("run", "${PREFIX}instrCtrl", 170, 74, 180, 20),
 
-        phoebusgen.widget.Label("time_lbl", "Time/Div [s/div]", 380, 124, 160, 24),
-        phoebusgen.widget.TextEntry("time", "KDSOX:timePerDiv", 540, 122, 120, 26),
+        phoebusgen.widget.Label("time_lbl", "Time/Div [s/div]", 220, 74, 70, 20),
+        phoebusgen.widget.TextEntry("time", "${PREFIX}timePerDiv", 290, 74, 90, 20),
 
-        phoebusgen.widget.Label("trig_lvl_lbl", "Trig Level [V]", 680, 124, 120, 24),
-        phoebusgen.widget.TextEntry("trig_lvl", "KDSOX:trigLevel", 800, 122, 120, 26),
+        phoebusgen.widget.Label("reclen_lbl", "RecLength:", 400, 74, 80, 20),
+        phoebusgen.widget.TextEntry("reclenS", "${PREFIX}recLengthS", 480, 74, 80, 20),
+        phoebusgen.widget.TextEntry("reclenR", "${PREFIX}recLengthR", 560, 74, 80, 20),
 
-        phoebusgen.widget.Label("wf_lbl", "CH1 Waveform", 20, 164, 140, 24),
-        phoebusgen.widget.XYPlot("wf_plot", 20, 190, 930, 150),
+        phoebusgen.widget.Label("trig_lvl_lbl", "Trig Level:", 660, 74, 70, 20),
+        phoebusgen.widget.TextEntry("trig_lvl", "${PREFIX}trigLevel", 730, 74, 70, 20),
+
+        phoebusgen.widget.Label("wf_lbl", "Waveforms", 10, 110, 960, 420),
+        phoebusgen.widget.XYPlot("wf_plot", 10, 130, 930, 430),
+
+        phoebusgen.widget.Label("scpi_lbl", "SCPI:", 10, 580, 50, 20),
+        phoebusgen.widget.TextEntry("scpiS", "${PREFIX}instrCmdS", 80, 580, 110, 20),
+        phoebusgen.widget.TextEntry("scpiR", "${PREFIX}instrCmdR", 200, 580, 760, 20),
     ]
-
     # Configure combo box choices directly for convenience.
     for w in widgets:
         if w.get_element_value("name") == "run":
@@ -51,11 +59,13 @@ def main() -> None:
     plot = next(w for w in widgets if w.get_element_value("name") == "wf_plot")
     x_axis = phoebusgen.widget.XYPlotXAxis()
     x_axis.title("Samples")
+    x_axis.auto_scale(True)
     y_axis = phoebusgen.widget.XYPlotYAxis()
     y_axis.title("Volts")
+    y_axis.auto_scale(True)
     trace = phoebusgen.widget.XYPlotTrace()
     trace.name("CH1")
-    trace.y_pv("KDSOX:c01Waveform")
+    trace.y_pv("${PREFIX}c01Waveform")
     plot.add_x_axis(x_axis)
     plot.add_y_axis(y_axis)
     plot.add_trace(trace)
@@ -64,7 +74,6 @@ def main() -> None:
 
     out = Path(__file__).with_name("simplescope.bob")
     screen.write_screen(str(out))
-
 
 if __name__ == "__main__":
     main()
